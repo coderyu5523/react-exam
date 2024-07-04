44507a03e25513b47bd55fdf442ebcf05e62d035
coderyu5523 <coderyu5523@naver.com>
2024-07-04 20:24:56 +0900

    기본 문법 정리

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 전개 연산자
+// let user = {name:"홍길동",age:20};
+// let user1 = {...user,age:21};
+// console.log(user);
+// console.log(user1);
+
+// filter 사용
+// const b = [4,5,6];
+
+// let b1 = b.filter((item)=>item !==5);
+
+// console.log(b);
+// console.log(b1);
+
+//map 사용
+// let postList = [
+//   {id : 1, title:"제목1",content:"내용1"},
+//   {id : 2, title:"제목2",content:"내용2"},
+//   {id : 3, title:"제목3",content:"내용3"}
+// ];
+// let post = postList.map((item)=>({
+//   ...item,
+//   content:"내용변경"
+// }));
+// console.log(postList)
+// console.log(post);
+
+let list =[1,2,3,4];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div>
+<div>1</div>
+<div>2</div>
+<div>3</div>
+{list.map((i)=>
+<h1>{i}</h1>
+)}
+
+  </div>
+)}
 
 export default App;
